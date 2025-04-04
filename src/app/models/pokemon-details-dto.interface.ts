eba1c8cbf8fe395bb590cf445db7d1adff12ffa1
A.Lejeune <acelyalejeune@gmail.com>
2025-04-04 21:26:42 +0200

    pokemon details

--- src/app/models/pokemon-details-dto.interface.ts
+++ src/app/models/pokemon-details-dto.interface.ts
@@ -1,3 +1,46 @@
 export interface PokemonDetailsDto {
+  id: number;
   name: string;
+  height: number;
+  weight: number;
+  base_experience: number;
+  types: PokemonType[];
+  abilities: PokemonAbility[];
+  stats: PokemonStat[];
+  sprites: PokemonSprites;
+}
+
+export interface PokemonType {
+  slot: number;
+  type: {
+    name: string;
+    url: string;
+  };
+}
+
+export interface PokemonAbility {
+  ability: {
+    name: string;
+    url: string;
+  };
+  is_hidden: boolean;
+}
+
+export interface PokemonStat {
+  base_stat: number;
+  effort: number;
+  stat: {
+    name: string;
+    url: string;
+  };
+}
+
+export interface PokemonSprites {
+  front_default: string;
+  back_default: string;
+  other?: {
+    "official-artwork": {
+      front_default: string;
+    };
+  };
 }
--- src/app/pages/pokemon-details/pokemon-details.component.ts
+++ src/app/pages/pokemon-details/pokemon-details.component.ts
@@ -1,24 +1,49 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { PokemonsService } from '../../services/pokemons.service';
-import { ActivatedRoute } from '@angular/router';
-import { AsyncPipe } from '@angular/common';
-import { PokemonDetailsDto } from '../../models/pokemon-details-dto.interface';
+import { Component, inject, OnInit } from "@angular/core";
+import { PokemonsService } from "../../services/pokemons.service";
+import { ActivatedRoute } from "@angular/router";
+import { AsyncPipe, TitleCasePipe } from "@angular/common";
+import { PokemonDetailsDto } from "../../models/pokemon-details-dto.interface";
+import { MatCardModule } from "@angular/material/card";
+import { MatDividerModule } from "@angular/material/divider";
+import { MatListModule } from "@angular/material/list";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { MatChipsModule } from "@angular/material/chips";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { MatProgressBarModule } from "@angular/material/progress-bar";
 
 @Component({
-  selector: 'app-pokemon-details',
+  selector: "app-pokemon-details",
   standalone: true,
-  imports: [AsyncPipe],
-  templateUrl: './pokemon-details.component.html',
-  styleUrls: ['./pokemon-details.component.scss'],
+  imports: [
+    AsyncPipe,
+    TitleCasePipe,
+    MatCardModule,
+    MatDividerModule,
+    MatListModule,
+    MatProgressSpinnerModule,
+    MatChipsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatProgressBarModule,
+  ],
+  templateUrl: "./pokemon-details.component.html",
+  styleUrls: ["./pokemon-details.component.scss"],
 })
 export class PokemonDetailsComponent implements OnInit {
   private readonly pokemonService = inject(PokemonsService);
   private readonly route = inject(ActivatedRoute);
 
-  public pokemonId = this.route.snapshot.params?.['id'];
+  public pokemonId = this.route.snapshot.params?.["id"];
   public pokemonDetails$ = this.pokemonService.getDetails(this.pokemonId);
 
   ngOnInit(): void {
-    console.log('Pokemon ID:', this.pokemonId);
+    console.log("Pokemon ID:", this.pokemonId);
+  }
+
+  getStatColor(value: number): string {
+    if (value >= 80) return "primary";
+    if (value >= 50) return "accent";
+    return "warn";
   }
 }
